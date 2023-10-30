0e7c9cb9f4ea25f503435ee7c62d2266a946098b
whalewalker <mongodbms@gmail.com>
2023-10-31 00:44:32 +0100

    project setup

---
+++ src/main/java/com/sliderbackend/SliderBackendApplication.java
@@ -0,0 +1,13 @@
+package com.sliderbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SliderBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SliderBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/sliderbackend/SliderBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sliderbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SliderBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
